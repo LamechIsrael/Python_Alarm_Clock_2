fa4a406105c4d97c7a73a7ec6882ce71b0c4ade7
Lamech A Israel <103595802+LamechIsrael@users.noreply.github.com>
2023-02-28 16:12:02 -0600

    Add files via upload

---
+++ Israel,Lamech_Mod1Ex02_AlarmClock2.py
@@ -0,0 +1,39 @@
+
+
+#########################################
+# CS 1110A - Programming in Python      #
+# Module 1 - Exercise 2 - Alarm Clock 2 #
+# Author: Lamech Israel                 #
+# Date:   01/10/2022                    #
+#########################################
+
+print('Alarm Clock, Part 2')
+
+# Input
+
+current_Time = int(input('\nWhat time is it now (in 24-hour notation):'))
+
+alarm_Hours = int(input('How many hours from now should the alarm go off?'))
+
+# Processing
+
+alarm_Hours *=100
+alarm_Days = int(alarm_Hours/2400)
+alarm_Remainder = alarm_Hours%2400
+
+alarm_Time = current_Time + alarm_Remainder
+
+if alarm_Time <1200 :
+    alarm_Time = '0' + str(alarm_Time)
+
+
+    
+#Output
+    
+
+print('The alarm clock will ring',alarm_Days,'days from now at',alarm_Time)
+
+print('\nDone!')
+    
+
+
